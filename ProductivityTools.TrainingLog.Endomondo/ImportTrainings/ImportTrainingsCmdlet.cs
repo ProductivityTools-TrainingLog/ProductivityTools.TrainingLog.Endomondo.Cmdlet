8a8f64d32790225f4c02cb0e5f843d7e9788bb92
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-01-21 21:34:31 +0100

    initial commit

---
+++ ProductivityTools.TrainingLog.Endomondo/ImportTrainings/ImportTrainingsCmdlet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.TrainingLog.Endomondo.ImportTrainings
+{
+    [Cmdlet("Import", "Trainings")]
+    class ImportTrainingsCmdlet: PSCmdlet.PSCmdletPT
+    {
+    }
+}
